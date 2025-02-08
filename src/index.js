469885d85aedc49b571dc7f39b7e328a2c5bf22a
Алексей Никитин <rysis17@yandex.ru>
2025-02-08 20:52:58 +0300

    fix 2 modified src/index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
+import fs from 'fs';
 import path from 'path';
-import process from 'process';
-import { readFileSync } from 'fs';
 import parse from './parse.js';
 import compareFiles from './comparefiles.js';
 import getFormat from './formatters/index.js';
 
-const getFilePath = (filepath) => path.resolve(process.cwd(), filepath);
-const getFileType = (filepath) => path.extname(filepath).slice(1);
-const readFile = (filepath) => readFileSync(getFilePath(filepath));
-const dataParse = (filepath, ext) => parse(filepath, ext);
+const dataParse = (filepath) => {
+  const absolutePath = path.resolve(process.cwd(), filepath);
+  const fileExtension = path.extname(filepath).slice(1);
+  const fileContent = fs.readFileSync(absolutePath, 'utf-8');
+  return parse(fileContent, fileExtension);
+};
 
 const gendiff = (filepath1, filepath2, format = 'stylish') => {
-  const ext1 = getFileType(filepath1);
-  const data1 = readFile(filepath1);
-  const ext2 = getFileType(filepath2);
-  const data2 = readFile(filepath2);
-  const obj1 = dataParse(data1, ext1);
-  const obj2 = dataParse(data2, ext2);
-  const difference = compareFiles(obj1, obj2);
+  const data1 = dataParse(filepath1);
+  const data2 = dataParse(filepath2);
+  const difference = compareFiles(data1, data2);
   return getFormat(difference, format);
 };
 
